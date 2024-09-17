d80f4acafdccdea669773838e520aaba3d443f73
A-Damien <ajd2024@gmail.com>
2024-09-17 18:51:43 -0400

    Added Movement to Object

---
+++ Assets/MovementScripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    //public Gun gun;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+
+    [Header("Dash Settings")]
+    [SerializeField] float dashSpeed = 15f;
+    [SerializeField] float dashDuration = 1f;
+    [SerializeField] float dashCoolDown = 1f;
+    bool isDash;
+    bool canDash;
+
+    private void Start(){
+        canDash = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDash){
+            return;
+        }
+        float moveX = Input.GetAxisRaw("Horizontal"); 
+        float moveY = Input.GetAxisRaw("Vertical"); 
+
+        /*
+        if(Input.GetMouseButtonDown(0)){
+            gun.Fire();
+        }
+
+        if(Input.GetMouseButtonDown(1)){
+            gun.FireBig();
+        }
+        */
+
+        if(Input.GetKeyDown(KeyCode.Space) && canDash){
+            StartCoroutine(Dash());
+        }
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate(){
+        if(isDash){
+            return;
+        }
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+    }
+
+    private IEnumerator Dash(){
+        canDash = false;
+        isDash = true;
+        rb.velocity = new Vector2(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDash = false;
+        yield return new WaitForSeconds(dashCoolDown);
+        canDash = true;
+    }
+}
