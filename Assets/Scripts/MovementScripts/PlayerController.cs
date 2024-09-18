2944a5237105ce33315109f55d0c69805de9aafd
A-Damien <ajd2024@gmail.com>
2024-09-18 15:01:47 -0400

    Altered Aiming while dashing

--- Assets/Scripts/MovementScripts/PlayerController.cs
+++ Assets/Scripts/MovementScripts/PlayerController.cs
@@ -46,19 +46,23 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate(){
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        
         if(isDash){
+            rb.rotation = aimAngle;
             return;
         }
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = aimAngle;
+
     }
 
     private IEnumerator Dash(){
         canDash = false;
         isDash = true;
         rb.velocity = new Vector2(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed);
+        
         yield return new WaitForSeconds(dashDuration);
         isDash = false;
         yield return new WaitForSeconds(dashCoolDown);
