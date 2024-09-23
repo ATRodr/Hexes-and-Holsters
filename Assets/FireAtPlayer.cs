e23c48900f6dbce9bb3a873cbaef21a93955355d
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2024-09-23 14:14:17 -0400

    Ranged enemy prefab now has LOS and obsticle is now static rigid body

--- Assets/FireAtPlayer.cs
+++ Assets/FireAtPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,12 @@ public class FireAtPlayer : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
     private float timer;
+    private GameObject player;
+    private bool hasLOS = false;
 
     void Start()
     {
-        
+        player  = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
@@ -18,8 +21,26 @@ public class FireAtPlayer : MonoBehaviour
         timer += Time.deltaTime;
         if (timer >= 2)
         {
-            timer = 0;
-            shoot();
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position, distance:Mathf.Infinity, layerMask:1);
+            if (ray.collider != null)
+            {
+                hasLOS = ray.collider.CompareTag("Player");
+                if (hasLOS)
+                {
+                    Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+                    Debug.Log("Player in sight");
+                    timer = 0;
+                    shoot();
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+                }
+            }
+            else
+            {
+                Debug.Log("raycast null");
+            }
         }
     }
 
