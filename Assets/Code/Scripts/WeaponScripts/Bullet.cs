82d7fe6cc833dc2f464fd19c1427a2cd400032fa
gavincoding <gavinroak03@gmail.com>
2024-10-19 06:09:30 -0400

    simplfied layers for easier debugging, broken chain Lighning broken navMesh

--- Assets/Code/Scripts/WeaponScripts/Bullet.cs
+++ Assets/Code/Scripts/WeaponScripts/Bullet.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    void start()
-    {
-        Debug.Log("Destroy");
-        Destroy(gameObject, 15f);
-    }
-    private void OnCollisionEnter2D(Collision2D collision){
 
-        if(collision.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent)){
-            enemyComponent.TakeDamage(1, gameObject);
+    // Boolean to check if it's a chain lightning bolt
+    public bool isChainLightning = false;
+    public GameObject chainLightningEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Enemy>(out Enemy enemyComponent))
+        {
+            if (isChainLightning)
+            {
+                Debug.Log($"Hit: {collision.gameObject.name}, Layer: {collision.gameObject.layer}");
+                enemyComponent.TakeDamage(0, gameObject);
+                Instantiate(chainLightningEffect, collision.collider.transform.position, Quaternion.identity);
+            }
+            else
+                enemyComponent.TakeDamage(1, gameObject);
+
         }
-        Destroy(gameObject);
-       
-        
+
+        Destroy(gameObject); // Destroy bullet after collision
     }
 }
--- Assets/Code/Scripts/WeaponScripts/Gun.cs
+++ Assets/Code/Scripts/WeaponScripts/Gun.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject boltPrefab;
     public Transform firePoint;
     public float fireForce = 30f;
+
+    public ChainLightningScript chainLightningEffect;
 
     public void Fire(){
         
@@ -14,6 +17,19 @@ public class Gun : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
         Destroy(bullet, 5f);
     }
+     public void FireLightning(){
+        
+        GameObject bullet = Instantiate(boltPrefab, firePoint.position, firePoint.rotation);
+
+        Bullet bulletScript = bullet.GetComponent<Bullet>();
+        bulletScript.isChainLightning = true;
+        
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
+
+
+        //Destroy(bullet, 5f);
+    }
+
 
 }
 
