7e47b49f2baaa120b654ae8b21c1a6ba2f95b08f
ATRodr <treyrod24@gmail.com>
2024-12-01 14:35:46 -0500

    enemies stay dead need to add reset

--- Assets/Level/Scenes/Menus/MainMenu.cs
+++ Assets/Level/Scenes/Menus/MainMenu.cs
@@ -7,11 +7,25 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+        ResetEnemyStates();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void ResetEnemyStates()
+    {
+        foreach (var ID in PlayerPrefs.GetString("EnemyID", "").Split(';'))
+        {
+            if (!string.IsNullOrEmpty(ID))
+            {
+                PlayerPrefs.DeleteKey(ID);
+            }
+        }
+        PlayerPrefs.Save();
+
     }
 }
