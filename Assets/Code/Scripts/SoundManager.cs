c02cf4b9d951f3b03ad28a2f9440f80ab58e11bf
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2024-12-01 11:55:15 -0500

    initial setup complete

---
+++ Assets/Code/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField] private AudioSource soundFxObject;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        // spawn in game object
+        AudioSource audioSource = Instantiate(soundFxObject, spawnTransform.position, Quaternion.identity);
+
+        // assign audio clip
+        audioSource.clip = audioClip;
+
+        // set volume
+        audioSource.volume = volume;
+
+        // play sound
+        audioSource.Play();
+
+        // get length of audio clip
+        float clipLength = audioSource.clip.length;
+
+        // destroy game object after clip length
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
